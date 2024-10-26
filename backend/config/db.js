d77f01562fa0498c29510635075a2e1f12bd8ae9
Hussien Y Shhade <113283040+Non00010110000@users.noreply.github.com>
2024-10-26 12:29:30 +0300

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+    console.log(`Successfully connnected to mongoDB 👍`);
+  } catch (error) {
+    console.error(`ERROR: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
