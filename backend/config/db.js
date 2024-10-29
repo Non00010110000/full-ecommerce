5859b07c2dbb21185ad6dddb4ddf7b6a23986fb7
Hussien Y Shhade <113283040+Non00010110000@users.noreply.github.com>
2024-10-29 14:04:09 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ecoomerce");
+    await mongoose.connect(process.env.Mongo);
     console.log(`Successfully connnected to mongoDB 👍`);
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
